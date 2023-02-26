ef13b249733b25c55b123b385b3c9659fd2c75f1
riceshower001 <125946802+riceshower001@users.noreply.github.com>
2023-02-27 03:44:35 +0900

    Add files via upload

---
+++ JavaScript_practiceII_object_what is an object.js
@@ -0,0 +1,17 @@
+// Let's define an object.
+
+// Please define the constant "character" and assign an object with the following two elements.
+
+// first element
+// property name: name
+// value: String "◆◆◆ ◆◆◆"
+
+// second element
+// property name: age
+// value: number "14"
+
+// Please define the constant "character" and assign the specified object.
+const character = {name:"◆◆◆ ◆◆◆", age:14};
+
+// Please output the value of character.
+console.log(character);
